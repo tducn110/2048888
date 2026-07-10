867a9edc8da9b0506a9c9e09d996e30b797c80d6
Nguyen Tam Duc <duc.nguyen240205@vnuk.edu.vn>
2026-07-10 17:03:05 +0700

    chore: remove tailwind boilerplate tech debt

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import path from 'path'
-import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 
@@ -25,10 +24,7 @@ export default defineConfig({
   },
   plugins: [
     figmaAssetResolver(),
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
     react(),
-    tailwindcss(),
   ],
   resolve: {
     alias: {
